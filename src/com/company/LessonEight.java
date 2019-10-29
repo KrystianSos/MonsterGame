48446fe4348ea4bcd399310aa25d9841f2866675
Krystian Sosnowski <kr.sosnowski@gmail.com>
2019-10-29 21:23:22 +0100

    movement complete

--- src/com/company/LessonEight.java
+++ src/com/company/LessonEight.java
@@ -1,21 +1,35 @@
 package com.company;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public class LessonEight {
 
     public static void main(String[] args) {
         MonsterTwo.buildBattleBoard();
 
-        char[][] tempBattleBoard = new char [10][10];
+        //char[][] tempBattleBoard = new char[10][10];
 
         MonsterTwo[] Monsters = new MonsterTwo[4];
 
         Monsters[0] = new MonsterTwo(1000, 20, 1, "Frank");
-        Monsters[0] = new MonsterTwo(500, 40, 2, "George");
-        Monsters[0] = new MonsterTwo(750, 30, 2, "Paul");
-        Monsters[0] = new MonsterTwo(800, 34, 1, "Hank");
+        Monsters[1] = new MonsterTwo(500, 40, 2, "George");
+        Monsters[2] = new MonsterTwo(850, 30, 2, "Paul");
+        Monsters[3] = new MonsterTwo(800, 34, 1, "Hank");
 
         MonsterTwo.redrawBoard();
 
+        for (MonsterTwo m : Monsters)
+        {
+
+            if(m.getAlive())
+            {
+                int arrayItemIndex = ArrayUtils.indexOf(Monsters, m);
+                m.moveMonster(Monsters, arrayItemIndex);
+            }
+
+        }
+
+        MonsterTwo.redrawBoard();
     }
 
 }
--- src/com/company/MonsterTwo.java
+++ src/com/company/MonsterTwo.java
@@ -6,41 +6,59 @@ public class MonsterTwo {
 
     static char[][] battleBoard = new char[10][10];
 
-    public static void buildBattleBoard()
-    {
-        for(char[] row : battleBoard)
-        {
-            Arrays.fill(row,'*');
+    public static void buildBattleBoard() {
+        for (char[] row : battleBoard) {
+            Arrays.fill(row, '*');
         }
     }
 
-    public static void redrawBoard()
-    {
+    public static void redrawBoard() {
         int k = 1;
-        while(k <= 30)
-        {
+        while (k <= 30) {
+            System.out.print("-");
+            k++;
+        }
+
+        System.out.println();
+        for (char[] row : battleBoard) {
+            for (char column : row) {
+                System.out.print("|" + column + "|");
+            }
+            System.out.println();
+        }
+
+        k = 1;
+        while (k <= 30) {
+            System.out.print("-");
+            k++;
+        }
+        System.out.println();
+    }
+
+    /*public static void redrawBoard() {
+        int k = 1;
+        while (k <= 30) {
             System.out.print("-");
             k++;
         }
         System.out.println();
 
-        for (int i = 0; i < battleBoard.length; i++)
-        {
-            for (int j = 0; j < battleBoard[i].length; j++)
-            {
+        for (int i = 0; i < battleBoard.length; i++) {
+            for (int j = 0; j < battleBoard[i].length; j++) {
                 System.out.print("|" + battleBoard[i][j] + "|");
             }
             System.out.println();
         }
 
         k = 1;
-        while(k <= 30)
-        {
+        while (k <= 30) {
             System.out.print("-");
             k++;
         }
         System.out.println();
     }
+
+     */
 
     public final String TOMBSTONE = "Here Lies a Dead Monster";
 
@@ -72,55 +90,121 @@ public class MonsterTwo {
         return alive;
     }
 
-    public void setHealth(int decreaseHealth)
-    {
+    public void setHealth(int decreaseHealth) {
         health = health - decreaseHealth;
-        if (health < 0)
-        {
+        if (health < 0) {
             alive = false;
         }
     }
 
-    public void setHealth(double decreaseHealth)
-    {
-        int intDecreaseHealth = (int)decreaseHealth;
+    public void setHealth(double decreaseHealth) {
+        int intDecreaseHealth = (int) decreaseHealth;
         health = health - intDecreaseHealth;
-        if (health < 0)
-        {
+        if (health < 0) {
             alive = false;
         }
     }
 
-    public MonsterTwo(int health, int attack, int movement, String name)
-    {
-        this.health = health;
-        this.attack = attack;
-        this.movement = movement;
-        this.name = name;
+    public void moveMonster(MonsterTwo[] monster, int arrayItemIndex) {
+        boolean isSpaceOpen = true;
 
-        int maxXBoardSpace = battleBoard.length;
-        int maxYBoardSpace = battleBoard[0].length;
+        int maxXBoardSpace = battleBoard.length - 1;
+        int maxYBoardSpace = battleBoard[0].length - 1;
 
-        int ranNumX, ranNumY;
+        while (isSpaceOpen) {
+            int randomeMoveDirection = (int) (Math.random() * 4);
+            int randMoveDistace = (int) (Math.random() * (this.getMovement() + 1));
 
-        do {
-            ranNumX = (int)(Math.random()*maxXBoardSpace);
-            ranNumY = (int)(Math.random()*maxYBoardSpace);
-        } while(battleBoard[ranNumX][ranNumY] != '*');
+            System.out.println(randMoveDistace + " " + randomeMoveDirection);
 
-        this.xPosition = ranNumX;
-        this.yPosition = ranNumY;
+            battleBoard[this.yPosition][this.xPosition] = '*';
 
-        this.nameChar1 = this.name.charAt(0);
+            if (randomeMoveDirection == 0) {
+                if ((this.yPosition - randMoveDistace) < 0) {
+                    this.yPosition = 0;
+                } else {
+                    this.yPosition = yPosition - randMoveDistace;
+                }
+            } else if (randomeMoveDirection == 1) {
+                if ((this.xPosition + randMoveDistace) > maxXBoardSpace) {
+                    this.xPosition = maxXBoardSpace;
+                } else {
+                    this.xPosition = xPosition + randMoveDistace;
+                }
+            } else if (randomeMoveDirection == 2) {
+                if ((this.yPosition + randMoveDistace) > maxYBoardSpace) {
+                    this.yPosition = maxYBoardSpace;
+                } else {
+                    this.yPosition = yPosition + randMoveDistace;
+                }
+            } else {
+                if ((this.xPosition - randMoveDistace) < 0) {
+                    this.xPosition = 0;
+                } else {
+                    this.xPosition = xPosition - randMoveDistace;
+                }
+            }
+
+            for(int i = 0; i < monster.length; i++)
+            {
+                if(i == arrayItemIndex)
+                {
+                    continue;
+                }
+
+                if(onMySpace(monster, i, arrayItemIndex))
+                {
+                    isSpaceOpen = true;
+                    break;
+                } else {
+                    isSpaceOpen = false;
+                }
+            }
+        }
 
         battleBoard[this.yPosition][this.xPosition] = this.nameChar1;
-        numOfMonsters++;
-    }
-
-    public MonsterTwo()
-    {
-        numOfMonsters++;
 
     }
+
+    public boolean onMySpace(MonsterTwo[] monster, int indexToChk1, int indexToChk2){
+        if ((monster[indexToChk1].xPosition)==(monster[indexToChk2].xPosition) && (monster[indexToChk1].yPosition)==(monster[indexToChk2].yPosition)){
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+
+    public MonsterTwo( int health, int attack, int movement, String name){
+            this.health = health;
+            this.attack = attack;
+            this.movement = movement;
+            this.name = name;
+
+            int maxXBoardSpace = battleBoard.length;
+            int maxYBoardSpace = battleBoard[0].length;
+
+            int ranNumX, ranNumY;
+
+            do {
+                ranNumX = (int) (Math.random() * maxXBoardSpace);
+                ranNumY = (int) (Math.random() * maxYBoardSpace);
+            } while (battleBoard[ranNumX][ranNumY] != '*');
+
+            this.xPosition = ranNumX;
+            this.yPosition = ranNumY;
+
+            this.nameChar1 = this.name.charAt(0);
+
+            battleBoard[this.yPosition][this.xPosition] = this.nameChar1;
+            numOfMonsters++;
+        }
+
+    public MonsterTwo() {
+            numOfMonsters++;
+
+        }
 
 }
+
