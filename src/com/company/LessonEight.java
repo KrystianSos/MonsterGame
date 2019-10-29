fde3545c6c38147e6d70e42d7926a2be7dd296dc
Krystian Sosnowski <kr.sosnowski@gmail.com>
2019-10-29 15:40:12 +0100

    Battle array with monsters

--- src/com/company/LessonEight.java
+++ src/com/company/LessonEight.java
@@ -7,6 +7,14 @@ public class LessonEight {
 
         char[][] tempBattleBoard = new char [10][10];
 
+        MonsterTwo[] Monsters = new MonsterTwo[4];
+
+        Monsters[0] = new MonsterTwo(1000, 20, 1, "Frank");
+        Monsters[0] = new MonsterTwo(500, 40, 2, "George");
+        Monsters[0] = new MonsterTwo(750, 30, 2, "Paul");
+        Monsters[0] = new MonsterTwo(800, 34, 1, "Hank");
+
+        MonsterTwo.redrawBoard();
 
     }
 
